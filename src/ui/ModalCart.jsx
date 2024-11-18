363af0f16ed1dbdda3a6b0b3018f0e63bcc5cb95
mowa49 <mostafafaraji94@gmail.com>
2024-11-18 09:23:18 +0100

    fixes 5

--- src/ui/ModalCart.jsx
+++ src/ui/ModalCart.jsx
@@ -8,7 +8,7 @@ function ModalCart({}) {
 
   if (!isModalOpen || items?.length === 0) return null;
   return (
-    <div className=" text-sm z-10 bg-white border-gray-950 shadow-2xl min-w-[15rem] min-h-[10rem] fixed top-[3.8rem] right-[10vw] backdrop-blur-md rounded-xl p-2 flex flex-col">
+    <div className="text-sm z-10 bg-white border-gray-950 shadow-2xl md:min-w-[18rem] min-h-[10rem] min-w-full right-[0] fixed top-[7rem] md:right-[26rem] backdrop-blur-md rounded-xl p-2 flex flex-col">
       <button
         className="self-end bg-transparent bg-none p-0 border-none"
         onClick={() => closeModal()}
@@ -17,23 +17,27 @@ function ModalCart({}) {
       </button>
       <div className=" flex justify-between w-full border-b-4 px-2">
         <p>Items</p>
-        <p>Quantity </p>
+        <p className="pr-3">Quantity </p>
       </div>
-      {items.map((item) => (
-        <div
-          key={item.id}
-          className="flex justify-between content-center items-center w-full px-4 border-b-2"
-        >
-          <p>{item.title.split(" ").slice(0, 3).join(" ")}</p>
-          <p> {item.quantity}</p>
-          <button
-            className="border-none w-2"
-            onClick={() => (handleRemove(item), console.log("click"))}
+      <div className="w-full">
+        {items.map((item) => (
+          <div
+            key={item.id}
+            className="flex justify-start content-center items-center w-full px-2 border-b-2"
           >
-            <DeleteForever />
-          </button>
-        </div>
-      ))}
+            <div className="w-full flex gap-4">
+              <p>{item.title.split(" ").slice(0, 3).join(" ")}</p>
+              <p> {item.quantity}</p>
+            </div>
+            <button
+              className="border-2 border-gray-200 p-1 h-8 w-8 flex items-center justify-center hover:bg-red-50 transition-colors"
+              onClick={() => (handleRemove(item), console.log("click"))}
+            >
+              <DeleteForever />
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
